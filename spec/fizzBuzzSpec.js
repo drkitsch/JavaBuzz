4bc0e1b9dd0fa8f7c7bf69c2447aa8c0f76de787
Harry Parsons <hparsons93@gmail.com>
2021-09-28 12:48:09 +0100

    tests complete

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -16,9 +16,20 @@ describe('Fizzbuzz', function() {
   });  
 
   describe('knows when a number is NOT', function() { 
-    it('divisible by 3', function() {
+    it('divisible by three', function() {
       
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('when playing says', function() {
+  
+    it('"Fizz" when a number is divisible by three', function() {
+      expect(fizzbuzz.says(3)).toEqual('Fizz');  
+    }); 
+
+    it('"Buzz" when a number is divisible by five', function() {
+      expect(fizzbuzz.says(5)).toEqual('Buzz');
     });
   });
   
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,27 @@
 class Fizzbuzz {
+  
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
   isDivisibleByThree(number) { 
-  return (number % 3 === 0); 
+  return this._isDivisibleBy(number, 3); 
+  }
+  isDivisibleByFifteen(number) { 
+    return this._isDivisibleBy(number, 15);
+  }
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+  says(number) {
+    if(this.isDivisibleByThree(number)) {
+      return "Fizz";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+   if (this.isDivisibleByFifteen(number)) {
+    return "Fizzbuzz";
+   }
+   return number;
   }
 }
